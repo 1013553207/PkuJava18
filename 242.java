4be3e3ee8837bcd0a5e9edf90c2dd2846b35568b
sunmingyue111 <sunmingyue111@163.com>
2016-01-04 22:11:16 +0800

    zuoye

---
+++ 242.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map=new HashMap<>();
+		
+		for(int i=0;i<s.length();i++){
+			if(map.containsKey(s.charAt(i)))
+				map.put(s.charAt(i), map.get(s.charAt(i))+1);
+			else{
+				map.put(s.charAt(i),1);
+			}
+		}
+		for(int j=0;j<t.length();j++){
+			if(!map.containsKey(t.charAt(j)))
+				return false;
+			else{
+				map.replace(t.charAt(j), map.get(t.charAt(j))-1);
+			}
+		}
+		for(Character c:map.keySet()){
+			if(map.get(c)!=0)
+				return false;
+		}
+		return true;
+
+}
+}
